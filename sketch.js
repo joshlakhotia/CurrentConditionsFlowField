948fee89a578f0445ba8c4a58fd07c025f5a637e
joshlakhotia <joshlakhotia@protonmail.com>
2023-02-23 11:39:35 -0700

    tied color of points to temperature

--- sketch.js
+++ sketch.js
@@ -2,15 +2,19 @@ let points = [];
 
 let weather;
 function preload() {
-  let url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.616363&lon=-111.829613&appid=e30f057024f399e0e4281bf280a03417'
+  let url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.616363&lon=-111.829613&appid=e30f057024f399e0e4281bf280a03417&units=imperial'
   weather = loadJSON(url);
 };
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(30);
   noiseDetail(1);
   angleMode(DEGREES);
+
+  console.log(weather);
   
   let density = weather.main.humidity;
   let space = width / density;
@@ -28,13 +32,31 @@ function draw() {
 
   for (let i = 0; i < points.length; i++) {
 
-    let r = map(points[i].x, 0, width, 50, 255);
-    let g = map(points[i].y, 0, height, 50, 255);
-    let b = map(points[i].x, 0, width, 255, 50);
+    let temp = weather.main.temp;
     let a = map(weather.clouds.all, 0, 100, 250, 50);
-    let mult = map (weather.wind.speed, 0, 5, 0.001, .01)
-
-    fill(r, g, b, a);
+    let mult = map (weather.wind.speed, 0, 40, 0.001, .01)
+    
+    if (temp > 100) {
+      let r = map(points[i].x, 0, width, 50, 255);
+      let g = map(points[i].y, 0, height, 50, 100);
+      let b = map(points[i].x, 0, width, 0, 0);
+      fill(r, g, b, a);
+    } else if (temp > 50) {
+      let r = map(points[i].x, 0, width, 100, 200);
+      let g = map(points[i].y, 0, height, 100, 255);
+      let b = map(points[i].x, 0, width, 0, 0);
+      fill(r, g, b, a);
+    } else if (temp > 0) {
+      let r = map(points[i].x, 0, width, 0, 50);
+      let g = map(points[i].y, 0, height, 50, 150);
+      let b = map(points[i].x, 0, width, 255, 100);
+      fill(r, g, b, a);
+    } else {
+      let r = map(points[i].x, 0, width, 0, 0);
+      let g = map(points[i].y, 0, height, 0, 50);
+      let b = map(points[i].x, 0, width, 255, 100);
+      fill(r, g, b, a);
+    }
 
     let angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 270)
 
